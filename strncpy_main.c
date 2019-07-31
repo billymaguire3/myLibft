22368b14dc81a1ff4d4bfa2df4af9f2b0aafaab3
William Maguire <wimaguir@e1z1r7p5.42.us.org>
2019-07-31 13:48:35 -0700

    .

---
+++ strncpy_main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strncpy_main.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wimaguir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/25 16:04:34 by wimaguir          #+#    #+#             */
+/*   Updated: 2019/07/31 13:45:27 by wimaguir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		main(void)
+{
+	char array[14];
+	char test[10];
+
+	printf("%s\n", ft_strncpy(array, "avbbvvvvvvvfffff", 12));
+	printf("%s\n", strncpy(test, "billy loves hockey", 2));
+	return (0);
+}
+
