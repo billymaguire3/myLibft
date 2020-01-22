903fe3908b894fb194eb17402de519ad03069531
William Maguire <billysmac@Williams-MacBook-Air.local>
2020-01-22 09:26:42 -0800

    fixed a weird header problem in ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,11 +6,8 @@
 /*   By: wimaguir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 17:15:54 by wimaguir          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2020/01/08 17:54:52 by wimaguir         ###   ########.fr       */
-=======
+/*   Updated: 2020/01/22 09:22:30 by wimaguir         ###   ########.fr       */
 /*   Updated: 2019/10/31 12:53:05 by wimaguir         ###   ########.fr       */
->>>>>>> bc74bcfe793049a001e2560329c5acbd1a9a8f1b
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +15,6 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-<<<<<<< HEAD
 	int		len;
 	char	*occur;
 
@@ -28,7 +24,6 @@ char	*ft_strrchr(const char *s, int c)
 		len--;
 	if (occur[len] == c)
 		return (&occur[len]);
-=======
 	int n;
 
 	n = ft_strlen(s);
@@ -36,6 +31,5 @@ char	*ft_strrchr(const char *s, int c)
 		n--;
 	if (s[n] == c)
 		return ((char *)&s[n]);
->>>>>>> bc74bcfe793049a001e2560329c5acbd1a9a8f1b
 	return (NULL);
 }
