692c18cf7ecf471cd5f9a89ea3457e0ce77f9634
William Maguire <billysmac@Williams-MacBook-Air.local>
2020-01-22 09:37:42 -0800

    fixed another problem with ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: wimaguir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 17:15:54 by wimaguir          #+#    #+#             */
-/*   Updated: 2020/01/22 09:22:30 by wimaguir         ###   ########.fr       */
+/*   Updated: 2020/01/22 09:28:58 by wimaguir         ###   ########.fr       */
 /*   Updated: 2019/10/31 12:53:05 by wimaguir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -24,12 +24,5 @@ char	*ft_strrchr(const char *s, int c)
 		len--;
 	if (occur[len] == c)
 		return (&occur[len]);
-	int n;
-
-	n = ft_strlen(s);
-	while (n > 0 && s[n] != c)
-		n--;
-	if (s[n] == c)
-		return ((char *)&s[n]);
 	return (NULL);
 }
