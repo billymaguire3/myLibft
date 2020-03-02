837b3eb549bddcf104b7a3219f0a179cbe2d3a03
William Maguire <wimaguir@student.42.us.org>
2020-03-02 15:40:06 -0800

    more bug fixes

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: wimaguir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/01 14:41:33 by wimaguir          #+#    #+#             */
-/*   Updated: 2020/02/24 16:16:19 by wimaguir         ###   ########.fr       */
+/*   Updated: 2020/03/02 15:39:43 by wimaguir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,13 @@
 int		ft_strcmp(const char *s1, const char *s2)
 {
 	int i;
+	unsigned char *str1;
+	unsigned char *str2;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (str1[i] != '\0' && str2[i] != '\0' && str1[i] == str2[i])
 		i++;
-	return (s1[i] - s2[i]);
+	return (str1[i] - str2[i]);
 }
